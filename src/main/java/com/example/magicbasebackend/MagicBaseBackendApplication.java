10c0d325dd6aee35a3b7e8359693eff0cd4f7f1d
DNY <dannylu1999@gmail.com>
2022-11-21 14:48:02 +0100

    Initial commit

---
+++ src/main/java/com/example/magicbasebackend/MagicBaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.magicbasebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicBaseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicBaseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/magicbasebackend/MagicBaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magicbasebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicBaseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
