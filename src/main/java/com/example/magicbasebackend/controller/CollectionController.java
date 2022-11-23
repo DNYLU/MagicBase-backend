d5bd77a202975fd8892cbfb0431ab6d2fb534a42
Danjie3000 <90595974+Danjie3000@users.noreply.github.com>
2022-11-23 09:59:00 +0100

    Collection delvis lavet

---
+++ src/main/java/com/example/magicbasebackend/controller/CollectionController.java
@@ -0,0 +1,31 @@
+package com.example.magicbasebackend.controller;
+
+import com.example.magicbasebackend.models.Collection;
+import com.example.magicbasebackend.services.CollectionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/collection")
+public class CollectionController {
+    private CollectionService collectionService;
+
+    public CollectionController(CollectionService collectionService) {
+        this.collectionService = collectionService;
+    }
+
+    @PostMapping("/add/{collectionId}/cards/{cardId}/user/{userName}")
+    public ResponseEntity<Collection> addCard(@RequestBody Collection collection,
+                                              @PathVariable("collectionId") Long collectionId,
+                                              @PathVariable("cardId") Long cardId,
+                                              @PathVariable("userName") String userName) {
+        return new ResponseEntity<>(collectionService.addCard(collection, collectionId, cardId, userName), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Collection> getCollectionById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(collectionService.getCollectionById(id), HttpStatus.OK);
+    }
+}
