cf4532ce060b82d3e876a53b14050826bfef20ec
DNY <dannylu1999@gmail.com>
2022-11-28 13:16:55 +0100

    Bruger har en collection gemt til sig når den oprettes

---
+++ src/test/java/com/example/magicbasebackend/services/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.magicbasebackend.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+    UserService userService;
+    @Test
+    void saveUser() {
+
+    }
+}
