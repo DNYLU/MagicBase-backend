e23855a75e69ed6da56dae21d76eb9c4006c2601
masp1234 <mart2c04@stud.kea.dk>
2022-11-28 09:39:54 +0100

    123

--- src/test/java/com/example/magicbasebackend/MagicBaseBackendApplicationTests.java
+++ src/test/java/com/example/magicbasebackend/MagicBaseBackendApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.magicbasebackend;
 
+import com.example.magicbasebackend.model.User;
+import com.example.magicbasebackend.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@SpringBootTest
+
 class MagicBaseBackendApplicationTests {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test() {
+        User user = userRepository.findByUsername("bobsen").get();
+        System.out.println(user);
+
+
+
     }
 
 }
